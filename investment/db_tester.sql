c2ba5388652540086171f767604ca2baec397967
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2026-03-25 15:01:14 +0300

    bugfix

--- investment/db_tester.sql
+++ investment/db_tester.sql
@@ -1,2 +1,5 @@
 SELECT * FROM price_history
 --TRUNCATE TABLE price_history RESTART IDENTITY;
+INSERT INTO investment_fund (code, quantity, buy_price, created_at) VALUES 
+SELECT * FROM investment_fund
+--TRUNCATE TABLE investment_fund;
